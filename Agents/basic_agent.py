9103a946464636871fb713356aa9cff2f0d30c2e
Sayemahamed <sayemahamed183@gmail.com>
2024-11-29 23:55:10 +0600

    developed first basic agent

---
+++ Agents/basic_agent.py
@@ -0,0 +1,21 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_ollama import ChatOllama
+
+
+class Agent:
+    def __init__(self):
+        self.llm = ChatOllama(
+            model="hf.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF", temperature=0.7
+        )
+
+    def respond(self, message):
+        return self.llm.invoke(
+            [
+                SystemMessage(content="You are a helpful assistant."),
+                HumanMessage(content=message),
+            ]
+        )
+
+
+agent = Agent()
+agent.respond("What is the capital of France?").to_json()["kwargs"]
