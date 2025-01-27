7a8e416e15c066325dff922a5b7247b13a8492fe
Sayemahamed <sayemahamed183@gmail.com>
2025-01-27 13:47:58 +0600

    Nearly Done

--- Summary_Keeping_Agent/agent.py
+++ Summary_Keeping_Agent/agent.py
@@ -1,9 +1,8 @@
-from tempfile import tempdir
-from typing import TypedDict, Annotated, Optional
+from typing import Literal, TypedDict, Annotated, Optional
 from langgraph.graph import START, END, StateGraph
 from langchain_ollama import ChatOllama
-from langgraph.graph.message import BaseMessage, add_messages, RemoveMessage
-from langchain_core.messages import SystemMessage
+from langgraph.graph.message import BaseMessage, add_messages
+from langchain_core.messages import  HumanMessage
 
 
 class State(TypedDict):
@@ -12,36 +11,54 @@ class State(TypedDict):
 
 
 agent = ChatOllama(
-    # model="qwen2.5:0.5b",
     model="granite3.1-moe",
     temperature=0.9,
 )
 
 
+def decider(state: State) -> Literal["agent", "summarize"]:
+    if len(state["messages"]) >= 5:
+        return "summarize"
+    else:
+        return "agent"
+
+
 def call_agent(state: State) -> State:
     summary = state.get("summary", "")
-    return {"messages": [agent.invoke(state["messages"])], "summary": summary}
+    agent_response = agent.invoke(state["messages"])
+    updated_messages = state["messages"] + [agent_response]
+    return {"messages": updated_messages, "summary": summary}
 
 
 def summarize(state: State) -> State:
-    summary: str = state.get("summary", "No Summary")  # type: ignore
-    print(summary)
-    print("=" * 80)
-    temp = agent.invoke(
-        state["messages"]
+    previous_summary: str = state.get("summary", "")  # type: ignore
+    summary = agent.invoke(
+        state["messages"][:4]
         + [
-            SystemMessage(content="Previous Summary: " + summary),
-            SystemMessage(
+            HumanMessage(
                 content="""Summarize the conversation between the user and the AI assistant.
 Cluster related ideas to condense the information.
-Eliminate repetition from the summary.
-Update the summary only when new information is present.
-"""
+Eliminate repetition from the summary."""
             ),
         ]
-    )
-    print(temp)
-    return {"summary": temp.content}  # type: ignore
+    ).content
+    if len(previous_summary) > 0:
+        summary = agent.invoke(
+            [
+                HumanMessage(
+                    content=f"""
+Merge the summary and keep key points.
+
+Previous summary: {previous_summary}
+
+New summary: {summary}
+"""
+                )
+            ]
+        ).content
+    # Remove the first 4 messages (excluding the last 2)
+    updated_messages = state["messages"][-2:]
+    return {"summary": summary, "messages": updated_messages}
 
 
 builder = StateGraph(State)
@@ -49,8 +66,8 @@ builder = StateGraph(State)
 builder.add_node("agent", call_agent)
 builder.add_node("summarize", summarize)
 
-builder.add_edge(START, "agent")
-builder.add_edge("agent", "summarize")
-builder.add_edge("summarize", END)
+builder.add_conditional_edges(START, decider)
+builder.add_edge("summarize", "agent")
+builder.add_edge("agent", END)
 
 graph = builder.compile()
